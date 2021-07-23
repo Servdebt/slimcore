04132fe52596ca5cf3ff98c08f6f1b2dd6dd1860
Rodrigo Alves <23285553+imrodrigoalves@users.noreply.github.com>
2021-07-23 11:01:53 +0100

    Corrections to implementation and organize class

--- src/Config/container.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use function DI\get;
-
-return [
-    'request' => get(\Psr\Http\Message\ServerRequestInterface::class),
-    'response' => get(\Psr\Http\Message\ResponseInterface::class),
-];

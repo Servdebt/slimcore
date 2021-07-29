ca3d84db2fe6e56d33c357f0672adc5b553a28cf
Rodrigo Alves <23285553+imrodrigoalves@users.noreply.github.com>
2021-07-29 12:32:19 +0100

    Correction

--- src/Handlers/Error.php
+++ src/Handlers/Error.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Servdebt\SlimCore\Handlers;
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Respect\Validation\Exceptions\NestedValidationException;
@@ -14,47 +15,44 @@ final class Error extends \Slim\Handlers\ErrorHandler
         bool $displayErrorDetails,
         bool $logErrors,
         bool $logErrorDetails
-    ): Response {
+    ): Response
+    {
         $app = app();
 
         $userInfo = [];
         if ($app->has('user')) {
             $user = $app->resolve('user');
             $userInfo = [
-                "id" => is_object($user) ? $user->id ?? $user->UserID ?? $user->userid ?? null : null,
+                "id"       => is_object($user) ? $user->id ?? $user->UserID ?? $user->userid ?? null : null,
                 "username" => is_object($user) ? $user->username ?? $user->Username ?? null : null,
             ];
         }
 
         $errorCode = $exception instanceof NestedValidationException ? 422 : 500;
-        $errorMsg  = $exception->getMessage() ." on ".  $exception->getFile() ." line ". $exception->getLine();
-        $messages  = $errorCode === 422 ? $exception->getMessages() : array_slice(preg_split('/\r\n|\r|\n/', $exception->getTraceAsString()), 0, 10);
+        $errorMsg = $exception->getMessage() . " on " . $exception->getFile() . " line " . $exception->getLine();
+        $messages = $errorCode === 422 ? $exception->getMessages() : array_slice(preg_split('/\r\n|\r|\n/', $exception->getTraceAsString()), 0, 10);
 
         // Log the message
         if (isset($this->logger) && $logErrors) {
             $this->logger->error($errorMsg, [
-                "trace"     => $messages,
-                "user"      => $userInfo,
-                "host"      => gethostname(),
-                "request"   => array_intersect_key($request->getServerParams(), array_flip([
+                "trace"   => $messages,
+                "user"    => $userInfo,
+                "host"    => gethostname(),
+                "request" => array_intersect_key($request->getServerParams(), array_flip([
                     "HTTP_HOST", "SERVER_ADDR", "REMOTE_ADDR", "SERVER_PROTOCOL", "HTTP_CONTENT_LENGTH",
-                    "HTTP_USER_AGENT", "REQUEST_METHOD", "REQUEST_URI", "CONTENT_TYPE", "REQUEST_TIME_FLOAT"
+                    "HTTP_USER_AGENT", "REQUEST_METHOD", "REQUEST_URI", "CONTENT_TYPE", "REQUEST_TIME_FLOAT",
                 ])),
                 // "query" => $request->getQueryParams(),
             ]);
         }
 
-        if ($request->getHeaderLine('Accept') === 'application/json' || !$displayErrorDetails) {
-            if (!$displayErrorDetails) {
-                $errorMsg = "Ops. An error occurred";
+        if (!$displayErrorDetails) {
+            $errorMsg = $errorCode === 422 ? "Validation error" : "Ops. An error occurred";
+            if ($errorCode !== 422) {
                 $messages = [];
             }
 
-            if(!$displayErrorDetails && $errorCode === 422){
-                $errorMsg = 'Unable to validate request';
-            }
-
-            return $app->error($errorCode, $errorMsg, $messages);
+            return app()->error($errorCode, $errorMsg, $messages);
         }
 
         if ($app->isConsole()) {

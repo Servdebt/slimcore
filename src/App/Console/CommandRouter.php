43b202d53cd37e70c961d9f610fc9d9f567aaf80
Nuno Chaves <nmpchaves@gmail.com>
2022-06-23 12:53:28 +0100

    packages updated. command router param to remove custom env names
    composer packages updated.
    command router param to remove custom env names

--- src/App/Console/CommandRouter.php
+++ src/App/Console/CommandRouter.php
@@ -13,7 +13,7 @@ class CommandRouter
         $this->app = $app;
     }
 
-    public function execute($argv)
+    public function execute($argv, ?array $environments = null)
     {
         ob_start();
         set_time_limit(0);
@@ -24,8 +24,10 @@ class CommandRouter
         // remove the cli.php param
         array_shift($cliCommandParts);
 
+        $envs = $environments ?? [App::DEVELOPMENT, App::STAGING, App::PRODUCTION];
+
         // if the first param is environment keyword, remove it (already processed at bootstrap)
-        if (in_array($cliCommandParts[0] ?? '', [App::DEVELOPMENT, App::STAGING, App::PRODUCTION])) {
+        if (in_array($cliCommandParts[0] ?? '', $envs)) {
             array_shift($cliCommandParts);
         }
 
@@ -33,16 +35,16 @@ class CommandRouter
         if (empty($cliCommandParts)) {
             return $this->app->resolveRoute([Help::class, "show"], []);
         }
-    
+
         // find where params start being key=val pairs
         for ($paramsStartPos=0; $paramsStartPos<count($cliCommandParts); $paramsStartPos++) {
             if (str_contains($cliCommandParts[$paramsStartPos], '=')) break;
         }
-    
+
         // split command from params
         $commandParts = array_slice($cliCommandParts, 0, $paramsStartPos);
         $paramsParts = array_slice($cliCommandParts, $paramsStartPos, count($cliCommandParts)-1);
-    
+
         // early return: if no command is given
         if (count($cliCommandParts) == 0) {
             $this->app->notFound();
@@ -54,7 +56,7 @@ class CommandRouter
 
         $method = array_pop($commandParts);
         $class = array_pop($commandParts);
-        
+
         $params = [];
         for ($i=0; $i<count($paramsParts); ++$i) {
             $parts = explode("=", $paramsParts[$i], 2);
@@ -66,7 +68,7 @@ class CommandRouter
 
         $namespace = "\\App\\Console". (count($commandParts) > 0 ? "\\".implode('\\', $commandParts) : "");
         $response = $this->tryResolveRoute($namespace, $class, $method, $params);
-        
+
         if ($response === false) {
             $namespace = "\\Servdebt\\SlimCore\\App\\Console". (count($commandParts) > 0 ? "\\".implode('\\', $commandParts) : "");
             $response = $this->tryResolveRoute($namespace, $class, $method, $params);

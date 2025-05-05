ddd5e4af0806e2310e8a1c5d474a3483720acd19
Nuno <nmpchaves@gmail.com>
2025-05-05 15:21:45 +0100

    redis parameters and options separation

--- src/ServiceProviders/Redis.php
+++ src/ServiceProviders/Redis.php
@@ -10,7 +10,20 @@ class Redis implements ProviderInterface
 
     public static function register(App $app, $serviceName, array $settings = []): void
     {
-        $redis = new RedisClient(new Client($settings));
+        $redis = new RedisClient(new Client(
+            [
+                'scheme' => $settings['scheme'] ?? 'tcp',
+                'path' => $settings['path'] ?? '',
+                'host' => $settings['host'],
+                'port' => $settings['port'] ?? 6379,
+            ], [
+                'parameters' => [
+                    'username' => $settings['username'] ?? 'default',
+                    'password' => $settings['password'] ?? '',
+                    'database' => $settings['database'],
+                ]
+            ]
+        ));
 
         $app->registerInContainer($serviceName, $redis);
     }
